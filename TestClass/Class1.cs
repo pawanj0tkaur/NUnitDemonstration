6e1f873ecf308ae8ebfa815f8b65172817668970
Dell <Dell@DESKTOP-O7TQ14S>
2024-02-13 18:00:39 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnitDemonstration;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace TestClass
+{
+    //Pawanjot Mutation Testing
+    public class Class1
+    {
+        [TestFixture]
+        public class TriangleTest1
+        {
+            [Test]
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+               ClassicAssert.AreEqual(expected, actual);
+
+            }
+        }
+    }
+}
